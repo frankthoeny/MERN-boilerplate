7e655b4d4d4cddb701422bb67933424cec4d3734
Frank Thoeny <frankthoeny@yahoo.com>
2018-11-03 13:26:21 -0700

    Added uglifyjs-3-webpack-plugin
    Webpack 4 removed uglifyjs from the package. uglifyjs-3-webpack-plugin is the new package to install.

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,6 +1,6 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-
+const UglifyJsPlugin = require("uglifyjs-3-webpack-plugin");
 const helpers = require('./helpers');
 const commonConfig = require('./webpack.common');
 
@@ -13,14 +13,14 @@ module.exports = merge(commonConfig, {
   },
 
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true
-      },
-      output: {
-        comments: false
-      }
+    new UglifyJsPlugin({
+      uglifyOptions: {
+         warnings: false,
+         ie8: false,
+         output: {
+           comments: false
+         }
+       }
     })
   ]
 });
